343e428fc30e6c5c59b23ed4fb7ea770da9dfa62
lissa0991 <162005217+lissa0991@users.noreply.github.com>
2024-03-12 00:33:00 -0700

    Update app.py

--- streamlit_lissa/app.py
+++ streamlit_lissa/app.py
@@ -1,103 +1,133 @@
-# Import Library
-import streamlit as st
 import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
-import os 
-
-# Nama : Rahmalia Nurhalissa
-# Submission Data Analyst ML-61
-
-# LOAD DATA
-
-
-
-# # Memanggil fungsi untuk memuat data
-# data = load_data()
-@st.cache_resource
-def load_data():
-    data = pd.read_csv("./day.csv")
-    return data
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+# Pengaturan awal
+sns.set(style='dark')
+st.set_option('deprecation.showPyplotGlobalUse', False)
+import datetime
+st.set_page_config(page_title="Air Quality Dashboard", layout="wide", initial_sidebar_state="auto")
 
 
-data = load_data()
-
-# TITLE DASHBOARD
-# Set page title
-st.title("Bike Share Dashboard Day")
+# Judul dashboard
+st.title('Dashboard Bike Sharing')
 
 
-st.sidebar.title("Dataset Bike Share")
-# Show the dataset
-if st.sidebar.checkbox("Show Dataset"):
-    st.subheader("Raw Data")
-    st.write(data)
+with st.sidebar:
+    # Menambahkan logo perusahaan 
+    name = st.text_input(label='Nama lengkap', value='')
+    st.write('Nama: ', name)
+  
+    number = st.number_input(label='Umur')
+    st.write('Umur: ', int(number), ' tahun')
 
-# Display summary statistics
-if st.sidebar.checkbox("Show Summary Statistics"):
-    st.subheader("Summary Statistics")
-    st.write(data.describe())
-
-# Show dataset source
-st.sidebar.markdown("[Download Dataset](https://www.kaggle.com/datasets/lakshmi25npathi/bike-sharing-dataset/code)")
-
-st.sidebar.markdown('**Weather:**')
-st.sidebar.markdown('1: Cerah, Sedikit awan, Berawan sebagian, Berawan sebagian')
-st.sidebar.markdown('2: Kabut + Berawan, Kabut + Awan pecah, Kabut + Sedikit awan, Kabut')
-st.sidebar.markdown('3: Salju Ringan, Hujan Ringan + Badai Petir + Awan Tersebar, Hujan Ringan + Awan Tersebar')
-st.sidebar.markdown('4: Hujan Lebat + Palet Es + Badai Petir + Kabut, Salju + Kabut')
+    date = st.date_input(label='Tanggal lahir', min_value= datetime.date(1900, 1, 1))
+    st.write('Tanggal lahir:', date)
+    # Mengambil start_date & end_date dari date_input
 
 
-# VISUALIZATION
-
-# create a layout with two columns
 col1, col2 = st.columns(2)
 
 with col1:
-    # Season-wise bike share count
-    # st.subheader("Season-wise Bike Share Count")
+    # Baca dataset
+    dataset_day = pd.read_csv("day.csv")
 
-    # Mapping dari angka ke label musim
-    season_mapping = {1: "Musim Semi", 2: "Musim Panas", 3: "Musim Hujan", 4: "Musim Dingin"}
-    data["season_label"] = data["season"].map(season_mapping)
+    # Plot scatter plot untuk hubungan antara suhu (temp) dan jumlah pengguna terdaftar (registered)
+    fig, ax = plt.subplots(figsize=(8, 6))  # Reduce the figure size
+    ax.scatter(dataset_day['temp'], dataset_day['registered'], color='skyblue', alpha=0.6)
+    ax.set_title('Hubungan antara Suhu dan Jumlah Pengguna Terdaftar')
+    ax.set_xlabel('Suhu (Celsius)')
+    ax.set_ylabel('Jumlah Pengguna Terdaftar')
+    ax.grid(True, linestyle='--', alpha=0.6)
 
-    season_count = data.groupby("season_label")["cnt"].sum().reset_index()
-    fig_season_count = px.bar(season_count, x="season_label",
-                              y="cnt", title="Jumlah Penyewa berdasarkan Musim")
-    st.plotly_chart(fig_season_count, use_container_width=True,
-                    height=400, width=600)
+    # Menampilkan plot menggunakan Streamlit
+    st.pyplot(fig)
+    st.write('Semakin tinggi kenaikan suhu maka semakin tinggi juga jumlah pengguna yang terdaftar, dapat dilihat pada grafik jumlah pendaftar tertinggi diantara 0.6 - 0.7 celcius, sebanyak 7000 pendaftar ')
 
-with col2:
-    # Weather situation-wise bike share count
-    # st.subheader("Weather Situation-wise Bike Share Count")
+    # Memfilter data untuk hari kerja
+    weekday_data = dataset_day[(dataset_day['holiday'] == 0) & (dataset_day['workingday'] == 1)]
 
-    weather_count = data.groupby("weathersit")["cnt"].sum().reset_index()
-    fig_weather_count = px.bar(weather_count, x="weathersit",
-                               y="cnt", title="Jumlah Penyewa berdasar Cuaca")
-    # Mengatur tinggi dan lebar gambar
-    st.plotly_chart(fig_weather_count, use_container_width=True,height=400, width=800)
+    # Menghitung jumlah total pengguna terdaftar dan pengguna sewaan pada hari kerja
+    total_registered_users = weekday_data['registered'].sum()
+    total_casual_users = weekday_data['casual'].sum()
+
+    # Visualisasi jumlah pengguna terdaftar dan pengguna sewaan pada hari kerja dalam bentuk grafik batang
+    fig, ax = plt.subplots(figsize=(10, 6))
+    categories = ['Pengguna Terdaftar', 'Pengguna Sewaan']
+    values = [total_registered_users, total_casual_users]
+    ax.bar(categories, values, color=['blue', 'orange'])
+    ax.set_ylabel('Jumlah Pengguna')
+    ax.set_title('Jumlah Pengguna Terdaftar dan Pengguna Sewaan pada Hari Kerja')
+    ax.grid(axis='y', linestyle='--', alpha=0.7)
+
+    # Menambahkan label nilai di atas bar
+    for i, v in enumerate(values):
+        ax.text(i, v + 10, str(v), ha='center', va='bottom', fontsize=10)
+
+    # Menampilkan plot menggunakan Streamlit
+    st.pyplot(fig)
+
+    # Menampilkan data hari kerja dan jumlah pengguna menggunakan Streamlit
+    st.write("Total pengguna terdaftar pada hari kerja:", total_registered_users)
+    st.write("Total pengguna sewaan pada hari kerja:", total_casual_users)
+
+with col2: 
+    dataset_day = pd.read_csv("day.csv")
+    # Membuat mapping untuk musim
+    season_mapping = {1: 'Winter', 2: 'Winter', 3: 'Spring', 4: 'Spring', 5: 'Spring', 6: 'Summer',
+                    7: 'Summer', 8: 'Summer', 9: 'Fall', 10: 'Fall', 11: 'Fall', 12: 'Winter'}
+    dataset_day['season'] = dataset_day['mnth'].map(season_mapping)
+
+    # Menghitung rata-rata sepeda yang disewa per musim
+    average_bikes_rented_per_season = dataset_day.groupby('season')['cnt'].mean()
+
+    # Menentukan musim dengan jumlah sepeda yang paling banyak disewa
+    most_rented_season = average_bikes_rented_per_season.idxmax()
+    total_rented_bikes_in_most_rented_season = average_bikes_rented_per_season.max()
 
 
-# weekday vs. Bike Share Count
-# st.subheader("Humidity vs. Bike Share Count")
-fig_weekday_chart = px.scatter(
-    data, x="weekday", y="cnt", title="hari kerja vs. Jumlah Penyewa")
-st.plotly_chart(fig_weekday_chart)
+    # Membuat plot menggunakan Matplotlib
+    fig, ax = plt.subplots(figsize=(10, 6))
 
-# holiday vs. Bike Share Count
-# st.subheader("holiday vs. Bike Share Count")
-fig_holiday_chart = px.scatter(
-    data, x="holiday", y="cnt", title="holiday vs. Jumlah Penyewa")
-st.plotly_chart(fig_holiday_chart)
+    # Menampilkan data dalam bentuk bar plot
+    average_bikes_rented_per_season.plot(kind='bar', color='skyblue', ax=ax)
+    ax.set_xlabel('Musim')
+    ax.set_ylabel('Rata-rata Jumlah Sepeda Disewa')
+    ax.set_title('Rata-rata Jumlah Sepeda Disewa per Musim')
+    ax.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.xticks(rotation=45)
 
-# Temperature vs. Bike Share Count
-# st.subheader("Temperature vs. Bike Share Count")
-fig_temp_chart = px.scatter(data, x="temp", y="cnt",
-                            title="Temperature vs. Jumlah Penyewa")
-st.plotly_chart(fig_temp_chart, use_container_width=True,
-                height=400, width=800)
+    # Menambahkan label nilai di atas bar
+    for i, v in enumerate(average_bikes_rented_per_season):
+        ax.text(i, v + 10, str(round(v, 2)), ha='center', va='bottom', fontsize=10)
 
-# Show data source and description
-st.sidebar.title("About")
-st.sidebar.info("Dashboard ini menampilkan visualisasi untuk sekumpulan data Bike Share. "
-                "Dataset ini mengandung informasi mengenai penyewaan sepeda berdasarkan berbagai variabel seperti musim, suhu, kelembaban, dan faktor lainnya.")
+    # Menampilkan plot menggunakan Streamlit
+    st.pyplot(fig)
+    # Menampilkan hasil menggunakan Streamlit
+    st.write("Musim dengan jumlah sepeda yang banyak disewa:", most_rented_season)
+    st.write("Jumlah sepeda yang disewa pada musim tersebut:", total_rented_bikes_in_most_rented_season)
+
+    # Memfilter data untuk hari libur
+    holiday_data = dataset_day[dataset_day['holiday'] == 1]
+
+    # Menghitung total jumlah pengguna terdaftar dan pengguna sewaan pada hari libur
+    total_registered_users_holiday = holiday_data['registered'].sum()
+    total_casual_users_holiday = holiday_data['casual'].sum()
+
+    # Visualisasi hubungan antara jumlah penyewa dan pendaftar dengan hari libur dalam bentuk bar chart
+    fig, ax = plt.subplots(figsize=(10, 6))
+    categories = ['Pengguna Terdaftar', 'Pengguna Sewaan']
+    values = [total_registered_users_holiday, total_casual_users_holiday]
+    ax.bar(categories, values, color=['blue', 'orange'])
+    ax.set_ylabel('Jumlah Pengguna')
+    ax.set_title('Hubungan Antara Jumlah Pengguna Terdaftar dan Pengguna Sewaan dengan Hari Libur')
+    ax.grid(axis='y', linestyle='--', alpha=0.7)
+
+    # Menambahkan label nilai di atas bar
+    for i, v in enumerate(values):
+        ax.text(i, v + 10, str(v), ha='center', va='bottom', fontsize=10)
+
+    # Menampilkan plot menggunakan Streamlit
+    st.pyplot(fig)
+    st.write("Total pengguna terdaftar pada hari libur:", total_registered_users_holiday)
+    st.write("Total pengguna sewaan pada hari libur:", total_casual_users_holiday)
